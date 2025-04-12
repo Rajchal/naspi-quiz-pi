42957350e3f75597a48cab13a13af6409e8201e0
Rajchal <rajchalanjal1@gmail.com>
2025-04-12 19:42:16 +0545

    using plain vanilla for performace

--- src/components/QuestionCard.jsx
+++
@@ -1,8 +0,0 @@
-export default function QuestionCard({ question }) {
-  return (
-    <div className="bg-white p-4 rounded shadow">
-      <p className="text-gray-800">{question}</p>
-    </div>
-  )
-}
-
