898da1c8f29ce7dc133421d4857df25984a88c3f
Rajchal <rajchalanjal1@gmail.com>
2025-04-13 09:52:05 +0545

    ui fixes

--- renderer.js
+++ renderer.js
@@ -6,12 +6,17 @@ async function fetchQuestion() {
     document.getElementById("question").textContent = data.question;
     const answers = document.getElementById("answers");
     answers.innerHTML = "";
+    const showAnswer = data.show;
 
     data.options.forEach((opt) => {
       const li = document.createElement("li");
       li.textContent = opt;
       answers.appendChild(li);
     });
+
+    if (showAnswer == "yes") {
+      document.getElementById("correctAnswer").textContent = data.correct;
+    }
   } catch (e) {
     console.error("Error fetching question:", e);
   }
