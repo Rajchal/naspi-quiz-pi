a2bcebc22e48d06506f2a6d52219d638d8e2b4bb
Rajchal <rajchalanjal1@gmail.com>
2025-04-15 13:12:30 +0545

    minor fix

--- renderer.js
+++ renderer.js
@@ -1,21 +1,37 @@
 async function fetchQuestion() {
   try {
-    const res = await fetch("http://192.168.1.121:5000");
+    const res = await fetch("http://192.168.85.216:5000");
     const data = await res.json();
 
     document.getElementById("question").textContent = data.question;
     const answers = document.getElementById("answers");
     answers.innerHTML = "";
     const showAnswer = data.show;
-
+    inde = 0;
     data.options.forEach((opt) => {
       const li = document.createElement("li");
       li.textContent = opt;
+      li.classList.add(
+        "bg-gray-700",
+        "p-4",
+        "rounded-lg",
+        "cursor-pointer",
+        "hover:bg-blue-500",
+        "transition",
+        "duration-300",
+        "text-lg"
+      );
       answers.appendChild(li);
+      inde++;
     });
-
     if (showAnswer == "yes") {
-      document.getElementById("correctAnswer").textContent = data.correct;
+      const popppElement = document.getElementById("poppp");
+      if (popppElement) {
+        popppElement.className = "text-green-400";
+        popppElement.style.display = "block"; // Ensure the element is visible
+      } else {
+        console.warn('Element with ID "poppp" not found.');
+      }
     }
   } catch (e) {
     console.error("Error fetching question:", e);
