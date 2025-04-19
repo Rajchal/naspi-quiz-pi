dbfb57f85c00a58f037bc1d3e0a34bc60a0a536c
Rajchal <rajchalanjal1@gmail.com>
2025-04-19 16:11:05 +0545

    fixed api links

--- renderer.js
+++ renderer.js
@@ -1,6 +1,6 @@
 async function fetchQuestion() {
   try {
-    const res = await fetch("http://192.168.1.17:5000");
+    const res = await fetch("http://192.168.1.17:5000/question-live");
     const data = await res.json();
     const ques = document.getElementById("question");
     ques.textContent = data.question;
@@ -40,6 +40,48 @@ async function fetchQuestion() {
   }
 }
 
+async function fetchChapter() {
+  try {
+    const res = await fetch("http://192.168.1.17:5000/chapters");
+    const data = await res.json();
+    const ques = document.getElementById("question");
+    ques.textContent = data.question;
+    ques.classList.remove("animate-pulse");
+    const answers = document.getElementById("answers");
+    answers.innerHTML = "";
+    const showAnswer = data.show;
+    inde = 0;
+    data.options.forEach((opt) => {
+      const li = document.createElement("li");
+      li.textContent = opt;
+      li.id = inde;
+      li.classList.add(
+        "bg-gray-700",
+        "p-4",
+        "rounded-lg",
+        "cursor-pointer",
+        "hover:bg-blue-500",
+        "transition",
+        "duration-300",
+        "text-lg"
+      );
+      answers.appendChild(li);
+      inde++;
+    });
+    if (showAnswer == "yes") {
+      const popppElement = document.getElementById(data.correct);
+      if (popppElement) {
+        popppElement.classList.remove("bg-gray-700");
+        popppElement.classList.add("bg-green-900");
+      } else {
+        console.warn(`Element with ID ${data.correct} not found.`);
+      }
+    }
+  } catch (e) {
+    console.error("Error fetching Chapters:", e);
+  }
+}
+
 const imageContainer = document.getElementById("image-container");
 const questionContainer = document.getElementById("question-container");
 
@@ -54,5 +96,47 @@ function showImage() {
   }, 5000);
 }
 
+async function fetchCommand() {
+  try {
+    const res = await fetch("http://192.168.1.17:5000/commands");
+    const data = await res.json();
+    const ques = document.getElementById("question");
+    ques.textContent = data.question;
+    ques.classList.remove("animate-pulse");
+    const answers = document.getElementById("answers");
+    answers.innerHTML = "";
+    const showAnswer = data.show;
+    inde = 0;
+    data.options.forEach((opt) => {
+      const li = document.createElement("li");
+      li.textContent = opt;
+      li.id = inde;
+      li.classList.add(
+        "bg-gray-700",
+        "p-4",
+        "rounded-lg",
+        "cursor-pointer",
+        "hover:bg-blue-500",
+        "transition",
+        "duration-300",
+        "text-lg"
+      );
+      answers.appendChild(li);
+      inde++;
+    });
+    if (showAnswer == "yes") {
+      const popppElement = document.getElementById(data.correct);
+      if (popppElement) {
+        popppElement.classList.remove("bg-gray-700");
+        popppElement.classList.add("bg-green-900");
+      } else {
+        console.warn(`Element with ID ${data.correct} not found.`);
+      }
+    }
+  } catch (e) {
+    console.error("Error fetching Chapters:", e);
+  }
+}
+
 showImage();
-setInterval(fetchQuestion, 3000);
+setInterval(fetchCommand, 3000);
