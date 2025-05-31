6ae8e055bc2d98d0bf35e4b257e2d0a06eb641bf
Rajchal <rajchalanjal1@gmail.com>
2025-05-31 13:59:58 +0545

    frame improvements

--- main.js
+++ main.js
@@ -3,8 +3,12 @@ const path = require('path');
 
 function createWindow () {
   const win = new BrowserWindow({
-    width: 1280,
-    height: 720,
+    width: 1920,
+    height: 1080,
+    resizable: false,
+    frame: false,
+    kiosk: true,
+    fullscreen: true,
     webPreferences: {
       nodeIntegration: true,
       contextIsolation: false,
