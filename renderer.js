53c3697a86e05901e791846dc052aadb42d55060
Rajchal <rajchalanjal1@gmail.com>
2025-06-11 10:15:02 +0545

    render fix

--- renderer.js
+++ renderer.js
@@ -1,138 +1,81 @@
-document.addEventListener("DOMContentLoaded", function() {
-  const imageContainer = document.getElementById("image-container");
-  const questionContainer = document.getElementById("question-container");
-  const pdfPage = document.getElementById("pdf-page");
+async function fetchQuestion(chapter_name) {
+  try {
+    const res = await fetch(`http://139.59.27.235:5000/live-quiz/${chapter_name}`);
+    const data = await res.json();
 
-  let switchFlag = false;
-  let chapter = 'none';
-  let pdf = 'none';
-  let myQuestion = [];
+    const ques = document.getElementById("question");
+    ques.textContent = data.question;
+    ques.classList.remove("animate-pulse");
 
-  async function fetchPdfImages(pdfName) {
-    try {
-      const res = await fetch("http://192.168.4.1:5000/pdf-images/" + pdfName);
-      const data = await res.json();
-      const display = data.display;
-      if (!display) {
-        imageContainer.style.display = "none";
-        pdfPage.style.display = "block";
-        switchFlag = false;
-        return;
+    const answers = document.getElementById("answers");
+    answers.innerHTML = "";
+    const showAnswer = data.show;
+    let inde = 0;
+    data.options.forEach((opt) => {
+      const li = document.createElement("li");
+      li.textContent = opt;
+      li.id = inde;
+      li.classList.add(
+        "bg-gray-700",
+        "p-4",
+        "rounded-lg",
+        "cursor-pointer",
+        "hover:bg-blue-500",
+        "transition",
+        "duration-300",
+        "text-lg"
+      );
+      answers.appendChild(li);
+      inde++;
+    });
+    if (showAnswer == "yes") {
+      const popppElement = document.getElementById(data.correct);
+      if (popppElement) {
+        popppElement.classList.remove("bg-gray-700");
+        popppElement.classList.add("bg-green-900");
+      } else {
+        console.warn(`Element with ID ${data.correct} not found.`);
       }
-      pdfPage.src = data.images;
-      pdfPage.style.display = "block";
-      pdfPage.alt = "PDF Page Image";
+    }
   } catch (e) {
-      console.error("Error fetching PDF images:", e);
-    }
+    console.error("Error fetching question:", e);
   }
+}
 
-  async function fetchQuestion(chapter_name) {
-    try {
-      const res = await fetch(`http://192.168.4.1:5000/live-quiz/${chapter_name}`);
-      const data = await res.json();
-      const display = data.display;
-      if (!display) {
-        imageContainer.style.display = "block";
-        questionContainer.style.display = "none";
-        switchFlag = false;
-        return;
-      } 
-      if (data.audio){
-        const audio =document.getElementById("audio");
-        audio.play();
+// Call fetchQuestion every 3 seconds
+setInterval(fetchQuestion('1-nero_quiz'), 3000);
 
-      }
-      else{
-        const audio =document.getElementById("audio");
-        audio.pause();
-        audio.currentTime = 0;
-      }
-      console.log("Question data:", data.question.options);
-      myQuestion = data.question.options;
-      const ques = document.getElementById("question");
-      ques.textContent = data.question.title;
-      ques.classList.remove("animate-pulse");
-
-      const answers = document.getElementById("answers");
-      answers.innerHTML = "";
-      myQuestion.forEach((opt) => {
-        const li = document.createElement("li");
-        li.textContent = opt;
-        li.classList.add(
-          "bg-gray-700",
-          "p-4",
-          "rounded-lg",
-          "cursor-pointer",
-          "hover:bg-blue-500",
-          "transition",
-          "duration-300",
-          "text-lg"
-        );
-        answers.appendChild(li);
-      });
-      if (data.question['imageRelativePath']!==null){
-        const img = document.getElementById("question-image");
-        img.style.display = "block";
-        img.alt = "Question Image";
-        img.src = "../api-test/uploads/"+chapter_name+"/"+data.question['imageRelativePath'].split('/')[2];
-        console.log("Image path:", img.src);
-        
-      }else {
-        const img = document.getElementById("question-image");
-        img.style.display = "none";
-      }
-      if (data.question['audioRelativePath']!==null){
-        const audio = document.getElementById("audio");
-        audio.style.display = "block";
-        audio.src = "../api-test/uploads/"+chapter_name+"/"+data.question['audioRelativePath'].split('/')[2];
-        console.log("Audio path:", audio.src);
-      }
-      else {
-        const audio = document.getElementById("audio");
-        audio.style.display = "none";
-      }
-    } catch (e) {
-      console.error("Error fetching question:", e);
-    }
+async function fetchChapter() {
+  try {
+    const res = await fetch("http://192.168.1.17:5000/chapters");
+    const data = await res.json();
+  } catch (e) {
+    console.error("Error fetching Chapters:", e);
   }
+}
 
-  function showImage() {
-    imageContainer.style.display = "block";
-    pdfPage.style.display = "none";
-    questionContainer.style.display = "none";
+const imageContainer = document.getElementById("image-container");
+const questionContainer = document.getElementById("question-container");
+
+function showImage() {
+  imageContainer.style.display = "block";
+  questionContainer.style.display = "none";
+
+  setTimeout(() => {
+    imageContainer.style.display = "none";
+    questionContainer.style.display = "block";
+    fetchQuestion();
+  }, 5000);
+}
+
+async function fetchCommand() {
+  try {
+    const res = await fetch("http://192.168.1.17:5000/commands");
+    const data = await res.json();
+  } catch (e) {
+    console.error("Error fetching Commands:", e);
   }
+}
 
-  async function fetchDisplay() {
-    try {
-      const res = await fetch("http://192.168.4.1:5000/display");
-      const data = await res.json();
-      if (data.display){
-        imageContainer.style.display = "none";
-        questionContainer.style.display = "block";
-        if (chapter){chapter = data.quizName;}
-        if (pdf){pdf=data.pdfName;}
-        switchFlag = true;
-      }
-    }
-    catch (e) {
-      console.error("Error fetching display:", e);
-    }
-  }
-
-  showImage();
-  function startup(){
-    if (!switchFlag) {
-      fetchDisplay();
-    }
-    else {
-      if (pdf !== 'none') {
-        fetchPdfImages(pdf);
-      }else if( chapter !== 'none') {
-        fetchQuestion(chapter);
-      }
-    }
-  }
-
-  setInterval(startup, 1000);
-});
+showImage();
+setInterval(fetchCommand, 3000);
